2f485564cd5d13db1cce04ebd8080a9c3426f18a
1Atlas1 <1Atlas1@protonmail.com>
2021-01-18 19:18:13 +0100

    Added comments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-function toggle() {
+function toggle_eye() {
+//getting PW input field
   var password = document.getElementById("LoginForm_Password");
   if (password.type === "password") {
+//unmasking password and toggle eye icon
     password.type = "text";
     document.getElementById("eye-pw").className = "far fa-eye";
   } else {
+//masking password and toggle eye icon
     password.type = "password";
     document.getElementById("eye-pw").className = "far fa-eye-slash";
   }
